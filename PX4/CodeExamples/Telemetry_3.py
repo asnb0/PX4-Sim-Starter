cf4c8a0a32649e757e2593c26e8379814ee24283
asnb <asnb1212@gmail.com>
2026-05-11 18:05:04 +0300

    adding telemetry_3 file

--- PX4/CodeExamples/Telemetry_3.py
+++ PX4/CodeExamples/Telemetry_3.py
@@ -1,52 +1,45 @@
 import asyncio
 from mavsdk import System
 
-
-async def position(drone: System):
-    async for position in drone.telemetry.position():
-        print(f"pos: {position}")
-
-async def battery(drone: System):
+async def battery_checker(drone: System):
+    print("Checking battery...")
     async for battery in drone.telemetry.battery():
-        print(f"battery: {battery.remaining_percent}")
+        if battery.remaining_percent < 30:
+            print(f"battery low returning to home")
+            await drone.action.return_to_launch()
 
-async def in_air(drone: System):
-    async for in_air in drone.telemetry.in_air():
-        print(in_air)
 
 async def run():
     drone = System()
+
     print("Connecting to PX4...")
     await drone.connect(system_address="udp://:14540")
-    print("drone connected")
 
-    async for connection in drone.core.connection_state():
-        print(f"is the drone connected: {connection.is_connected}")
-        if connection.is_connected:
-            print(f"drone connected")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("Drone connected!")
             break
 
-    home = await anext(drone.telemetry.home())
-    print(f"drone home {home}")
 
     print("Arming...")
     await drone.action.arm()
-
     async for armed in drone.telemetry.armed():
         print(f"armed: {armed}")
         if armed:
             print("Drone armed!")
             break
+
+
     print("Taking off...")
     await drone.action.takeoff()
-
     async for state in drone.telemetry.landed_state():
         print(f"after takeoff state: {state}")
         if state == state.IN_AIR:
             print("Drone took off!")
             break
 
-    # Land
+    await asyncio.create_task(battery_checker(drone))
+
     print("Landing...")
     await drone.action.land()
     async for state in drone.telemetry.landed_state():
@@ -54,7 +47,6 @@ async def run():
         if state == state.ON_GROUND:
             print("Drone landed!")
             break
-
 
 asyncio.run(run())
 
